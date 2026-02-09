482f5cef4a2489a6e287d7f2b2fc1a3e5f75e9a7
Karen Handy <karen.handy@cmcss.net>
2026-02-09 00:04:05 -0600

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,38 @@
+def main():
+    #Collect Input for each
+    #Variable: first Prompt: Child's First Name:
+    
+    #Variable: last Prompt: Child's Last Name:
+    
+    print(f"Camper's Name: {first} {last}")
+
+    #Variable: birth Prompt: In what year was {first} {last} born:
+    
+    print(f"Birth Year: {birth}")
+
+    #Variable: days Prompt: How many days will {first} attend?
+    
+    print(f"Camp Duration: {days} days")
+
+    #Variable: p_first Prompt: Parent's First Name:
+    
+    #Variable: p_last Prompt: Parent's Last Name:
+    
+    print(f"Parent's Name: {p_first} {p_last}")
+
+    #Variable: phone Prompt: Parent's Phone #:
+    
+    print(f"Phone Number: {phone}")
+
+    #Variable: street Prompt: Street Address:
+    
+    #Variable: city Prompt: City:
+    
+    #Variable: state Prompt: State Abbreviation:
+    
+    #Variable: zip Prompt: Zip Code:
+    
+    print(f"Address:\n{street}\n{city}, {state} {zip}")
+
+if __name__ == "__main__":
+    main()
