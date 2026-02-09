8ec7c8880b8ae1fe2330df569001e487b15a84fe
Karen Handy <karen.handy@cmcss.net>
2026-02-09 00:05:34 -0600

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,40 @@
+from data import first, last, birth, days, p_first, p_last, phone, street, city, state, zip_code
+
+# For each variable convert to the appropriate data type.
+
+
+# Not all variables will need conversion.
+
+
+# first
+
+
+# last
+
+
+# birth
+
+
+# days
+
+
+# p_first
+
+
+# p_last
+
+
+# phone
+
+
+# street
+
+
+# city
+
+
+# state
+
+
+# zip_code
+
